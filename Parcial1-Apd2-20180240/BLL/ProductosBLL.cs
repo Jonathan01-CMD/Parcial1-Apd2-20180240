5a32a43f1cbc432ba47cfc31eaef0088dd1cb7f8
Jonathan001-cmd <77310361+Jonathan001-cmd@users.noreply.github.com>
2021-06-02 19:26:18 -0400

    He terminado la  BLL de producto

--- Parcial1-Apd2-20180240/BLL/ProductosBLL.cs
+++ Parcial1-Apd2-20180240/BLL/ProductosBLL.cs
@@ -1,14 +1,166 @@
-﻿using Parcial1_Apd2_20180240.DAL;
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial1_Apd2_20180240.DAL;
 using Parcial1_Apd2_20180240.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace Parcial1_Apd2_20180240.BLL
 {
     public class ProductosBLL
     {
+       public static bool Guardar(Productos productos)
+        {
+            if (!Existe(productos.ProductoId))
+                return Insertar(productos);
+            else
+                return Modificar(productos);
+        }
+        public static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+
+            try
+            {
+                encontrado = contexto.Producto.Any(e => e.ProductoId == id);
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return encontrado;
+        }
+        private static bool Insertar(Productos productos)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Producto.Add(productos);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+        private static bool Modificar(Productos productos)
+        {
+            Contexto contexto = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                contexto.Entry(productos).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var producto = contexto.Producto.Find(id);
+
+                if(producto != null)
+                {
+                    contexto.Producto.Remove(producto);
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+        public static Productos Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Productos productos;
+
+            try
+            {
+                productos = contexto.Producto.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return productos;
+
+        }
         
+        public static List<Productos> GetProductos()
+        {
+            List<Productos> lista = new List<Productos>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                lista = contexto.Producto.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+        public static List<Productos>GetList(Expression<Func<Productos, bool>> criterios)
+        {
+            List<Productos> lista = new List<Productos>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                lista = contexto.Producto.Where(criterios).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+
     }
 }
