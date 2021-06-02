aca0b2b72a889d45c96c2dd57d0f9abf7b929489
Jonathan001-cmd <77310361+Jonathan001-cmd@users.noreply.github.com>
2021-06-02 18:27:28 -0400

    Modelo Productos

--- Parcial1-Apd2-20180240/Model/Productos.cs
+++ Parcial1-Apd2-20180240/Model/Productos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,22 @@ namespace Parcial1_Apd2_20180240.Model
 {
     public class Productos
     {
+        [Key]
+        [Required(ErrorMessage ="El ID es incorecto")]
+        public int ProductoId { get; set; }
+
+        [Required(ErrorMessage = "Descripsion no se puede dejar vacia")]
+        public string Descripsion   { get; set; }
+        
+        [Required(ErrorMessage = "El dato Existencia es incorecto")]
+        public int Existencia { get; set; }
+
+        [Required(ErrorMessage = "El dato Costo es incorecto")]
+        public int Costo { get; set; }
+
+        [Required(ErrorMessage = "El dato Inventario es incorecto")]
+        public int Inventario { get; set; }
+
+
     }
 }
