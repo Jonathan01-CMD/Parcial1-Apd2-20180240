3dd695e4f164e3a11f5453c202ff4176781c3ddd
Jonathan001-cmd <77310361+Jonathan001-cmd@users.noreply.github.com>
2021-06-02 11:59:05 -0400

    Add project files.

---
+++ Parcial1-Apd2-20180240/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial1_Apd2_20180240.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_Apd2_20180240.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Productos> Producto { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source =Data\BaseDeDatos.db");
+        }
+    }
+}
---
+++ Parcial1-Apd2-20180240/Model/Productos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_Apd2_20180240.Model
+{
+    public class Productos
+    {
+    }
+}
